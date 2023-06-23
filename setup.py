d4dde47050d8c9cbac38514111f2002b7bfc87e1
Cary Carter <ccarterdev@gmail.com>
2023-06-23 12:53:07 -0500

    forgot

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='logfunc',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     install_requires=[],
     author='Cary Carter',
