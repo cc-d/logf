4b53fdbb7af1b0ec7bdfd2359bf305ae37bf2b7b
Cary Carter <ccarterdev@gmail.com>
2023-06-23 12:49:07 -0500

    1.1 bby

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='logfunc',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     install_requires=[],
     author='Cary Carter',
