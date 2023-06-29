9191e4cdb1314d14ef608317960f566de0763650
Cary Carter <ccarterdev@gmail.com>
2023-06-29 16:54:27 -0500

    version increase with env var support new pypy packages published too

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='logfunc',
-    version='1.2.0',
+    version='1.3.0',
     packages=find_packages(),
     install_requires=[],
     author='Cary Carter',
